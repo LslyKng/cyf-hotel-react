ea30e3fd753bf9f94f24cdd7dd44d72eee5ac329
LslyKng <prezzles@gmail.com>
2018-05-05 12:41:59 +0100

    added image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Bookings from './containers/Bookings.js';
-import Footer from './components/Footer.js';
-import './App.css';
+import Bookings from "./containers/Bookings.js";
+import Footer from "./components/Footer.js";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">CYF Hotel</header>
+        <header className="App-header">Welcome to CYF Hotel</header>
+        <img
+          src="https://codeyourfuture.io/static/media/cyf_brand.fbcea877.png"
+          className="image"
+        />
+
         <Bookings />
         <Footer />
       </div>
